67041aa9b744fc8fc078aecb5259aedcd3e00021
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2025-04-13 10:39:23 +0100

    Update Armbg.js

--- plugins/Armbg.js
+++ plugins/Armbg.js
@@ -22,7 +22,7 @@ cmd({
   category: "utility",
   use: ".rmbg [reply to image]",
   filename: __filename
-}, async (client, message, { reply, quoted }) => {
+}, async (conn, client, message, { reply, quoted }) => {
   try {
     // Check if quoted message exists and has media
     const quotedMsg = quoted || message;
