17a784645fa3ca3fb216f4f5dd577d7b8d3ce416
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2025-04-13 08:44:15 +0100

    Update tool-converter.js

--- plugins/tool-converter.js
+++ plugins/tool-converter.js
@@ -35,7 +35,7 @@ cmd({
         // Send result
         await client.sendMessage(from, {
             image: imageBuffer,
-            caption: "> Powered By JawadTechX 🤍",
+            caption: "> Powered By DavidTechX 🤍",
             mimetype: 'image/png'
         }, { quoted: message });
 
